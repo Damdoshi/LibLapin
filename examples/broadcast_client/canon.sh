3b2f6641562485e0a6a512dc6287f12cb97affaa
Damdoshi <damdoshi@hotmail.com>
2016-09-11 20:28:28 +0200

    Scratch

--- examples/broadcast_client/canon.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-man gcc | ./broadcast_client 127.0.0.1 5000
--- include/lapin_enum.h
+++
@@ -1,9 +0,0 @@
-/*
-**
-*/
-
-#ifndef			__LAPIN_ENUM_COMPAT_H__
-# define		__LAPIN_ENUM_COMPAT_H__
-# include		"lapin/basic.h"
-#endif	/*		__LAPIN_ENUM_COMPAT_H__	*/
-
