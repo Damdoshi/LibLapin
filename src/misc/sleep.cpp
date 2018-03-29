f629f5fcfe90203fbcae7b75a28d4e2935537a3b
Jason Brillante <jbrillante@anevia.com>
2018-03-29 20:23:59 +0200

    Fix sleep for MinGW.

--- src/misc/sleep.cpp
+++ src/misc/sleep.cpp
@@ -13,6 +13,7 @@ unsigned long long	bunny_usleep(unsigned long long	delay)
 
 t_bunny_time		bunny_sleep(t_bunny_time	delay)
 {
+#if			!( _WIN32 || __WIN32__ )
   struct timespec	duration;
   struct timespec	remain;
 
@@ -21,4 +22,15 @@ t_bunny_time		bunny_sleep(t_bunny_time	delay)
   if (nanosleep(&duration, &remain))
     return (remain.tv_sec * 1e9 + remain.tv_nsec);
   return (0);
+#else
+  t_bunny_time		prev;
+  t_bunny_time		elap;
+
+  prev = bunny_get_time();
+  sf::Sleep(sf::microseconds(delay / 1000));
+  if ((elap = bunny_get_time() - prev) > delay)
+    return (0);
+  return (delay - elap);
+#endif
 }
+
