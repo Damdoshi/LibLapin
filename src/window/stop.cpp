0e90f726005bb239da48b64cacbf32e7dda0bfc5
Damdoshi <damdoshi@hotmail.com>
2020-02-18 17:24:25 +0100

    Fix a memory error in window deletion.

--- src/window/stop.cpp
+++ src/window/stop.cpp
@@ -9,7 +9,7 @@ void			bunny_stop(t_bunny_window	*window)
 {
   struct bunny_window	*win = (struct bunny_window*)window;
 
-  free((void*)win->window_name);
+  bunny_free((void*)win->window_name);
   win->window->close();
   delete win->window;
   delete win;
