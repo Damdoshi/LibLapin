64fa4b7e4601d6bb41599300ed566b9b74621a4e
Damdoshi <damdoshi@hotmail.com>
2024-09-09 23:45:24 +0200

    Gravitation program is good enough.

--- misc/programs/toys/gravitation/src/main.c
+++ misc/programs/toys/gravitation/src/main.c
@@ -1,15 +1,22 @@
 
 #include	<lapin.h>
 
+typedef struct s_bvap
+{
+  long double x;
+  long double y;
+} very_accurate_position;
+
 typedef struct	s_par
 {
-  t_bunny_accurate_position pos;
-  t_bunny_accurate_position spd;
-  double	weight;
+  very_accurate_position pos;
+  very_accurate_position spd;
+  long double	weight;
+  unsigned int	col;
 }		t_par;
 t_bunny_window *win;
-t_par		par[3];
-double		grav = 6.67430e-11;
+t_par		par[40];
+long double	grav = 6.67430e-11;
 
 t_bunny_response key(t_bunny_event_state state, t_bunny_keysym sym, void *a)
 {
@@ -22,7 +29,6 @@ t_bunny_response key(t_bunny_event_state state, t_bunny_keysym sym, void *a)
     grav *= 2;
   if (sym == BKS_DOWN)
     grav /= 2;
-  printf("Grav %f\n", grav);
   return (GO_ON);
 }
 
@@ -32,12 +38,15 @@ t_bunny_response loop(void *n)
   for (int i = 0; i < NBRCELL(par); ++i)
     for (int j = i + 1; j < NBRCELL(par); ++j)
       {
-	double dist = sqrt(pow(par[i].pos.y - par[j].pos.y, 2) + pow(par[i].pos.x - par[j].pos.x, 2));
-	double strength = (grav * par[i].weight * par[j].weight) / (dist * dist);
-	double ang = atan2(par[i].pos.y - par[j].pos.y, par[i].pos.x - par[j].pos.x);
+	long double dist = sqrtl(powl(par[i].pos.y - par[j].pos.y, 2) + powl(par[i].pos.x - par[j].pos.x, 2));
+	long double strength = (grav * par[i].weight * par[j].weight) / (dist * dist);
+	long double ang = atan2l(par[j].pos.y - par[i].pos.y, par[j].pos.x - par[i].pos.x);
 
-	par[i].spd.x += cos(ang) * strength;
-	par[i].spd.y += sin(ang) * strength;
+	par[i].spd.x += cosl(ang) * strength;
+	par[i].spd.y += sinl(ang) * strength;
+
+	par[j].spd.x += cosl(ang + M_PI) * strength;
+	par[j].spd.y += sinl(ang + M_PI) * strength;
       }
   for (int i = 0; i < NBRCELL(par); ++i)
     {
@@ -47,7 +56,15 @@ t_bunny_response loop(void *n)
       par[i].pos.y += par[i].spd.y;
       if (par[i].pos.y < 0 || par[i].pos.y > win->buffer.height)
 	par[i].spd.y *= -1;
+
+      // Ralentissement (même si c'est pas realiste)
+      par[i].spd.x *= 0.999;
+      par[i].spd.y *= 0.999;
     }
+  par[0].pos.x = 1440 / 2;
+  par[0].pos.y = 900 / 2;
+  par[0].spd.x = 0;
+  par[0].spd.y = 0;
   return (GO_ON);
 }
 
@@ -59,8 +76,12 @@ t_bunny_response display(void *n)
     {
       t_bunny_position pos = {(int)par[i].pos.x, (int)par[i].pos.y};
       t_bunny_position rad = {5, 5};
+      t_bunny_position brad = {20, 20};
 
-      bunny_set_disk(&win->buffer, pos, rad, WHITE, WHITE, 0);
+      if (i == 0)
+	bunny_set_disk(&win->buffer, pos, brad, par[i].col, WHITE, 0);
+      else
+	bunny_set_disk(&win->buffer, pos, rad, par[i].col, WHITE, 0);
     }
   bunny_display(win);
   return (GO_ON);
@@ -75,14 +96,16 @@ int		main(void)
   par[0].pos.y = 900 / 2.0;
   par[0].spd.x = 0;
   par[0].spd.y = 0;
-  par[0].weight = 1e18;
+  par[0].weight = 1e7;
+  par[0].col = WHITE;
   for (i = 1; i < NBRCELL(par); ++i)
     {
       par[i].pos.x = rand() % 1440;
       par[i].pos.y = rand() % 900;
       par[i].spd.x = (rand() % 2000) / 1000.0 - 1.0;
       par[i].spd.y = (rand() % 2000) / 1000.0 - 1.0;
-      par[0].weight = 1e9;
+      par[i].weight = 1e5;
+      par[i].col = rand() | BLACK;
     }
 
   bunny_set_key_response(key);
