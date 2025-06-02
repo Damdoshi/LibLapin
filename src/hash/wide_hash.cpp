663a2fa54c399b30bb7afa14b6021a49a943bc93
Jason Brillante 'Damdoshi <damdoshi@hotmail.com>
2025-06-02 13:25:23 +0200

    Bring wide hash for real.

--- src/hash/wide_hash.cpp
+++ src/hash/wide_hash.cpp
@@ -1,9 +1,19 @@
 // Jason Brillante "Damdoshi"
-// Hanged Bunny Studio 2014-2018
+// Hanged Bunny Studio 2014-2025
 //
 // Lapin library
 
 #include		"lapin_private.h"
+
+static uint8_t		squash(t_bunny_hash		h)
+{
+  uint8_t		*ptr = (uint8_t*)&h;
+  uint8_t		res = 0;
+
+  for (size_t i = 0; i < sizeof(h); ++i)
+    res ^= ptr[i];
+  return (res);
+}
 
 void			bunny_wide_hash(t_bunny_hash_algorithm hash,
 					const void	*to_hash,
@@ -11,9 +21,17 @@ void			bunny_wide_hash(t_bunny_hash_algorithm hash,
 					void		*storage,
 					size_t		slen)
 {
-  (void)hash;
-  (void)to_hash;
-  (void)hlen;
-  (void)storage;
-  (void)slen;
+  char			*out = (char*)storage;
+  t_bunny_hash		ohash[2];
+  size_t		i;
+
+  ohash[0] = ohash[1] = bunny_hash(hash, to_hash, hlen);
+  for (i = 0; i < slen; ++i)
+    {
+      out[i] = squash(ohash[0]);
+      ohash[0] = bunny_hash(hash, ohash, sizeof(ohash));
+      if ((i & 7) == 7 || (out[i] & 3) == 3)
+	ohash[1] ^= ohash[0];
+    }
 }
+
