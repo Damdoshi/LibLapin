75ff705dbb2510e6825e80ed6c25120513f039eb
Damdoshi <damdoshi@hotmail.com>
2025-08-28 11:57:53 +0200

    Remove database module wanabe. Edit debian package conf.

--- build_package.sh
+++ build_package.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 dpkg-buildpackage -us -uc -d
-mkdir conf/incoming
-(cd conf/incoming/ && reprepro -b . includedeb stable ../../../liblapin*.deb)
+#mkdir conf/incoming
+#(cd conf/incoming/ && reprepro -b . includedeb stable ../../../liblapin*.deb)
 
